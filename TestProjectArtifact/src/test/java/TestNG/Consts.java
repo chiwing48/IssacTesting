46233771111073ede3ba65defe76e051de316ad8
chiwing48 <chiwing48@users.noreply.github.com>
2018-02-14 11:51:04 +0800

    Trial1

---
+++ TestProjectArtifact/src/test/java/TestNG/Consts.java
@@ -0,0 +1,11 @@
+package TestNG;
+
+public final class Consts {
+	
+	/**
+	 * This is the Google Sheet ID
+	 */
+	public static final String SPREADSHEETID = "1LIz_0gKy-BjKrkUbM2RAo7cxtV3PiQszgHOJdxtn54I";
+	
+	
+}
